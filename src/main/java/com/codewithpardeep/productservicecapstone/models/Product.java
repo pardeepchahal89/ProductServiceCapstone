3e3795fc1bc9148d8ee41dab93743fb75f6e5de6
Pardeep Chahal <pardeepchahal89@gmail.com>
2025-05-05 16:59:29 +0530

    Add product and category models

---
+++ src/main/java/com/codewithpardeep/productservicecapstone/models/Product.java
@@ -0,0 +1,16 @@
+package com.codewithpardeep.productservicecapstone.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Category category;
+
+}
